e6db4f70da26a888b39daa685fcd2c255d1df2d7
laurette <dalmorolaurette@orange.fr>
2022-01-29 17:42:01 +0100

    organisation et comprehension de la page d'accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,61 @@
-    async function getPhotographers() {
-        const reponse = await fetch ("data/photographers.json")
-        const photographers = await reponse.json()
-        console.log (photographers)
-        
-        return photographers
+async function getPhotographers () {                                  //récupération du JSON
+    let response = await fetch ("data/photographers.json")
+    let photographers = await response.json()
+    return photographers
     }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+// CREATION DES CONSTANTES ET DES ELEMENTS AJOUTES DANS HTML ET QUI RECUPERENT LES INFOS JSON
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
+    function photographerFactory(data){                                 //1ère fonction qui permet d'englober le return vers la fonction getUserCardDom
+        
+        function getUserCardDOM() {                                     //2ème fonction qui crée les constantes et les éléments
+        const {portrait, name, city, country, tagline, price} = data;
+        const picture = `assets/photographers/${portrait}`;
+    
+
+        const article = document.createElement( "article" );
+        const img = document.createElement( "img" );
+        const h2 = document.createElement( "h2" );
+        const h3 = document.createElement("h3");
+        const h4 = document.createElement("h4");
+        const p = document.createElement("p");
+
+      // CREATION DU LIEN ENTRE INDEX.HTML ET PHOTOGRAPHER.HTML
+        article.addEventListener("click", ()=>{
+        window.location.href=`photographer.html?`;      //${id}   SERA A METTRE EN LIAISON AVEC L'ID DE CHAQUE PHOTOGRAPHE     
+                                                        //A REVOIR CAR LE LIEN DOIT ETRE SUR LA PHOTO ET LE NOM UNIQUEMENT 
+        })                                                
+
+        
+        img.setAttribute("src",picture)
+        h2.textContent = name;
+        h3.textContent = city +" "+ country;
+        h4.textContent = tagline;
+        p.textContent = price + "€/jour";
+        
+        article.appendChild(img);                                   //Mettre les nouveaux éléments en enfant d'article
+        article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(p);
+        return (article);
+    }
+    return{getUserCardDOM}
+    }
+
+    async function displayData(photographers) {                     // fonction qui cible l'endroit et fait apparaitre les nouveaux éléments
+        const photographersSection = document.querySelector(".photographer_section");   //cible l'endroit du DO
+
+        photographers.forEach((photographer) => {                   //récupérer la réponse du JSON const photographer pour chaque photographe
+            const photographerModel = photographerFactory (photographer);//création de la constante qui récupère la 1ère fonction
+            const userCardDOM = photographerModel.getUserCardDOM();         // création de la constante qui récupère la 1ère et la 2ème fonction
+            photographersSection.appendChild(userCardDOM);                  //Mise en place des 2 fonctions en tant qu'enfant
+            console.log (userCardDOM)
         });
     };
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+    async function init(){                                              //Fonction mettre en display le résultat de toutes les fonctions
+        const {photographers} = await getPhotographers();               //const photographers récupère le tableau du JSON
         displayData(photographers);
-    };
-    
-    init();
-    
+    }
+  init ()                                                               //Active la fonction init
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,35 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+function pagePhotographe(data) {
+    const { portrait, name, city, country, tagline } = data;
+    const picture = `assets/photographers/${portrait}`;
+    const img = document.createElement( 'img' );
+    const h2 = document.createElement( 'h2' );
+    const h3 = document.createElement("h3");
+    const h4 = document.createElement("h4");
+
+        img.setAttribute("src",picture)
+        h2.textContent = name;
+        h3.textContent = city +" "+ country;
+        h4.textContent = tagline;
+
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        return (pagePhotographe);
+}
+
+async function displayPhotographe(photographers) {
+    const infosPhotographe = document.querySelector(".infos_photographe");
+
+    photographers.forEach((photographer) => {
+        const pagePhotographe = pagePhotographe();
+        infosPhotographe.appendChild(pagePhotographe);
+    });
+};
+
+
+
+
+
