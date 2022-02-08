8fba3399787930f53bb0bdc6caa68e463df9749e
laurette <dalmorolaurette@orange.fr>
2022-02-08 23:28:11 +0100

    enfin apparition page du photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,10 +1,9 @@
 
 
-async function getPhotographers (id) {                                  
-    const response = await fetch ("data/photographers.json")
-    const photographers = await response.json()
-    const photographer = photographers.photographers[id];
-    return photographers
+async function getPhotographers () {                                  
+    const response = await fetch ("data/photographers.json");
+    const data = await response.json();
+    return data;
 }
 
 let url = new URL(window.location.href);
@@ -12,17 +11,21 @@ let photographerId = url.searchParams.get("id");
 
 function displayData(photographers) {                      
 
-    photographers.forEach((photographerId => {
-        const photographerModel = photographerFactory (photographerId);
+    photographers.forEach(photographer=> {
+        if (photographer.id == photographerId){
+
+        const photographerModel = photographerFactory (photographer);
         const userCardDOM = photographerModel.getUserCardDOM(); 
-    }));
-    if (photographers.id == photographerId){
-        displayData()
-    } 
+    }});
 }
 
-async function display(photographerId){                                              
-    const {photographers} = await getPhotographers(photographerId);
+async function display(){                                              
+    const {photographers, media} = await getPhotographers();
     displayData(photographers);
 }
-display (photographerId)                              
+display ();                              
+/*async function getMedia(){
+    const response = await fetch ("data/photographers.json")
+    const medias = await response.json()
+    medias = medias.media
+}*/
