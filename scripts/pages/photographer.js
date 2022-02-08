3633f346fdae574e442381148ecbd6d231a81a27
laurette <dalmorolaurette@orange.fr>
2022-02-08 16:40:55 +0100

    apparition de plusieurs photographe sur page photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,25 +1,27 @@
 
 
-async function getPhotographers () {                                  //récupération du JSON
+async function getPhotographers (id) {                                  
     const response = await fetch ("data/photographers.json")
     const photographers = await response.json()
+    const photographer = photographers.photographers[id];
     return photographers
 }
 
-async function displayData(photographers) {                     // fonction qui cible l'endroit et fait apparaitre les nouveaux éléments
+let url = new URL(window.location.href);
+let photographerId = url.searchParams.get("id");
+
+async function displayData(photographers) {                      
   const infosPhotographe = document.querySelector(".infos_photographe"); 
           //cible l'endroit du DO
 
-  photographers.forEach((photographer) => {                   //récupérer la réponse du JSON const photographer pour chaque photographe
-    const photographerModel = photographerFactory (photographer);//création de la constante qui récupère la 1ère fonction
-    const userCardDOM = photographerModel.getUserCardDOM();         // création de la constante qui récupère la 1ère et la 2ème fonction
-    infosPhotographe.appendChild(userCardDOM);
-                             //Mise en place des 2 fonctions en tant qu'enfant
+  photographers.forEach((photographerId) => {                   
+    const photographerModel = photographerFactory (photographerId);
+    const userCardDOM = photographerModel.getUserCardDOM(); 
   });
 };
 
-async function display(){                                              //Fonction mettre en display le résultat de toutes les fonctions
-  const {photographers} = await getPhotographers();               //const photographers récupère le tableau du JSON
+async function display(photographerId){                                              
+  const {photographers} = await getPhotographers(photographerId);               
   displayData(photographers);
 }
-display ()                              
+display (photographerId)                              
