39fdf7246a33276314243f0cf3c55f59ea8761d5
laurette <dalmorolaurette@orange.fr>
2022-02-10 14:26:50 +0100

    css  carte photo, modal et menu choix

--- scripts/factories/fact_medias.js
+++ scripts/factories/fact_medias.js
@@ -4,23 +4,31 @@ function mediaFactory(data){
         
     function getUserMediaDOM() {                                     
         const {image, title, likes} = data;
-        const images = document.querySelector(".images"); 
-
-    
-        const h2 = document.createElement( "h2" );
-        const h3 = document.createElement("h3");
-
-        h2.textContent = title;
-        h3.textContent = likes;
-    
-        images.appendChild(h2);
-        images.appendChild(h3);
-
+        const carteMedia = document.querySelector(".carte_media"); 
+        
         const photo = `assets/photographers/${image}`;
         const img = document.createElement( "img" );
         img.setAttribute("src",photo);
         img.setAttribute("alt",title);
-        images.appendChild(img);
+        carteMedia.appendChild(img);
+
+        const infoPhoto = documment.createElement("div");
+        infoPhoto.classList.add("info_photo");
+        const h2 = document.createElement( "h2" );
+        const h3 = document.createElement("h3");
+        const coeur = document.createElement("i")
+        coeur.classList.add("fas fa-heart")
+
+        h2.textContent = title;
+        h3.textContent = likes;
+        
+        
+        carteMedia.appendChild("infoPhoto");
+        infoPhoto.appendChild(h2);
+        infoPhoto.appendChild(h3);
+        infoPhoto.appendChild(i);
+
+        
     }
     return{getUserMediaDOM}
 }
