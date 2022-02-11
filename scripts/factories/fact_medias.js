0f4fcd6f6e0f6f44f08744f7962942e9424b66b6
laurette <dalmorolaurette@orange.fr>
2022-02-11 14:19:50 +0100

    ajout code pour videos

--- scripts/factories/fact_medias.js
+++ scripts/factories/fact_medias.js
@@ -3,18 +3,31 @@
 function mediaFactory(data){                                 
         
     function getUserMediaDOM() {                                     
-        const {image, title, likes} = data;
+        const {image, video, title, likes} = data;
         const cartesMedias = document.querySelector(".cartes_medias");
         
         const carteMedia = document.createElement("article");
         carteMedia.classList.add ("carte_media");
         cartesMedias.appendChild(carteMedia);
         
-        const photo = `assets/photographers/${image}`;
-        const img = document.createElement( "img" );
-        img.setAttribute("src",photo);
-        img.setAttribute("alt",title);
-        carteMedia.appendChild(img);
+        if("video" in data){
+            const photoVideo = document.createElement("video");
+            const mp4 = `assets/photographers/${video}`;
+            const source = document.createElement("source");
+            source.setAttribute("src",mp4);
+            source.setAttribute("alt", title);
+            source.setAttribute("type", "video/mp4");
+            carteMedia.appendChild(photoVideo);
+            photoVideo.appendChild(source);
+        }
+        else {
+            const photo = `assets/photographers/${image}`;
+            const img = document.createElement( "img" );
+            img.setAttribute("src",photo);
+            img.setAttribute("alt",title);
+            carteMedia.appendChild(img);
+        }
+        
 
         
         const infoPhoto = document.createElement("div");
